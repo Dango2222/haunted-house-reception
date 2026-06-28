ee9cc3f00cb6b40088a56adf4ea73b0512634017
Dango2222 <kdango2222@gmail.com>
2026-06-28 19:18:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,83 @@
-body{
-    text-align: center;
-    background-color: #f0f2f5;    
-    color: #000;
-    font-size: 60px;
-    margin: 20px;
-    font-family: "Yu Gothic", "Hiragino Sans", sans-serif;
-    user-select: none;
+//count
+let waiting = 0;
+
+function update(){
+    document.getElementById("count").textContent =
+    `現在${waiting}組待ち`;
 }
 
+update();
 
-#buttons{
-    display: flex;
-    justify-content: center;
-    gap: 100px;
+document.getElementById("plus").onclick = function(){
+    waiting++;
+    update();
 }
 
-#count{
-    font-size: 100px;
-    margin: 0px;
+document.getElementById("minus").onclick = function(){
+
+    if(waiting > 0){
+        waiting--;
+    }
+
+    update();
 }
 
-#bt{
-    margin-bottom: 0px;
-    font-size: 70px;
-}
-#timer{
-    margin-top: 10px;
-    font-size: 100px;
-}
+//timer
+const stopButton = document.getElementById("stop");
 
-#start{
-    margin-bottom: 30px;
-    width: 90%;
-    max-width: 500px;
-    height: 200px;
-    font-size: 50px;
-    background-color: rgb(132, 255, 255);
-}
-#stop{
-    margin-bottom: 30px;
-    width: 90%;
-    max-width: 500px;
-    height: 150px;
-    font-size: 50px;
-    background-color: rgb(190, 255, 255);
-}
+let seconds = 0;
+let timerId = null;
 
-button{
-    width: 180px;
-    height: 150px;
-    max-width: 40vw;
-    font-size: 100px;
-    border-radius: 20px;
-    border: none;
-    background-color: #ccced2;
-    color: #000000;
-    touch-action: manipulation;
-    user-select: none;
-    cursor:pointer;
-    -webkit-tap-highlight-color: transparent;
-    -webkit-touch-callout:none;
+function updateTimer() {
+
+    let min = Math.floor(seconds / 60);
+    let sec = seconds % 60;
+
+    document.getElementById("timer").textContent =
+        `${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
+
+}
+function startTimer() {
+    stopButton.textContent = "ストップ";    
+
+    if (timerId !== null) return; // すでに動いている
+
+    timerId = setInterval(function () {
+        seconds++;
+        updateTimer();
+    }, 1000);
+}
+function stopTimer() {
+
+    clearInterval(timerId);
+
+    timerId = null;
 }
 
-button:hover {
-  opacity: 0.7;
-}
+document.getElementById("start").addEventListener("click", function () {
+    seconds = 0;
+    updateTimer();
+    startTimer();
+    if(waiting > 0){
+        waiting--;
+        update();
+    }
+});
 
-p{
-    margin-bottom: 10px;
-}
+document.getElementById("stop").addEventListener("click", function () {  
+    if (seconds > 0){  
+        if (timerId === null) {
 
-.card{
-    background-color: #f0f2f5;  
-    border-radius: 15px;
+            // 停止中 → 再開
+            startTimer();
+            
+        } else {
 
-    display: flex;
+            // 動作中 → 停止
+            stopTimer();
+            stopButton.textContent = "スタート";
 
-    flex-direction: column;   /* ←縦に並べる */
+        }
+    }
 
-    justify-content: center;  /* ←上下中央 */
-
-    align-items: center;      /* ←左右中央 */
-
-    margin: auto;
-
-    box-shadow: 5px 5px 10px rgb(164, 164, 164);
-
-    margin-bottom: 100px;
-    width:90%;
-    max-width:1000px;
-}
-
-#waitcount{
-    padding: 100px 0;
-}
-#timers{
-    padding: 50px 0;
-}
+});
